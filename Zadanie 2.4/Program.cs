2e98e9cc2a47b72204e14ecdf8f4f3b7acad3a31
VitalyIP <flatronmetal01@mail.ru>
2022-01-14 11:32:30 +0700

    Pochekytov 2.4

---
+++ Zadanie 2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter2.Task4;
+
+
+    namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter2.Task4.Program.run();
+        }
+    }
+}
